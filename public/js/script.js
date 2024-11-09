6c5a9707bb69034ec5289e63a651164d9586616b
mrsajadpp <mrsajadpp@gmail.com>
2024-11-09 23:33:55 +0530

    mobile menu added

--- public/js/script.js
+++ public/js/script.js
@@ -1,33 +1,11 @@
-console.log(`
-░█▀▀░▀█▀░█▀█░█▀▀░█▀▀░█▀█░█▀▀░▀█▀░█▀▀░░░░█░█░█░█░▀▀█
-░█░░░░█░░█░█░█▀▀░█░░░█▀█░▀▀█░░█░░█▀▀░░░░▄▀▄░░█░░▄▀░
-░▀▀▀░▀▀▀░▀░▀░▀▀▀░▀▀▀░▀░▀░▀▀▀░░▀░░▀▀▀░▀░░▀░▀░░▀░░▀▀▀
-    
-Welcome to Cinecast 🎬 
-Brought to you by Grovix Lab, Cinecast is an AI-powered platform dedicated to transforming the way you interact with text. With advanced Natural Language Processing, Cinecast provides:
-    
-🔹 Paragraph Generation  
-🔹 Description Generation  
-🔹 Text Summarization  
-🔹 And much more!
-    
-⚠️ Important: Do not paste anything in this console for your security.
-    
-Explore Cinecast and see how AI can make words work for you. Enjoy the experience!`);
+let navBtn = document.getElementById("navBtn");
+let closeBtn = document.getElementById("closeBtn");
+let navBar = document.getElementById("navBar");
 
-
-let menuIcon = document.getElementById("menuIcon");
-let menuBar = document.getElementById("menuBar");
-let menuSvg = document.getElementById("menu");
-
-menuIcon.addEventListener("click", () => {
-    menuBar.classList.toggle('hidden');
+navBtn.addEventListener("click", () => {
+    navBar.classList.toggle("open");
 });
 
-document.addEventListener('click', function (e) {
-    e = e || window.event;
-    var target = e.target || e.srcElement,
-        text = target.textContent || target.innerText;
-
-    if (menuIcon != target && menuSvg != target) menuBar.classList.add('hidden');
-}, false);
+closeBtn.addEventListener("click", () => {
+    navBar.classList.toggle("open");
+});
