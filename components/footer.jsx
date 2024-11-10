aed072b06d00b9820ff768dc195882cd838482b1
mrsajadpp <mrsajadpp@gmail.com>
2024-11-10 10:39:14 +0530

    footer  fixed

--- components/footer.jsx
+++ components/footer.jsx
@@ -17,37 +17,38 @@ const Footer = () => {
                 </div>
 
                 <ul className="footer__nav">
-                    <li className="nav__item">
-                        <h2 className="nav__title">Links</h2>
+                    {/* <li className="nav__item">
+                        <h2 className="nav__title"><b>Links</b></h2>
                         <ul className="nav__ul">
                             <li><a href="/">Home</a></li>
-                            <li><a href="/trending">Trending</a></li>
-                            <li><a href="/categories">Categories</a></li>
+                            <li><a href="/reasearche">Researche</a></li>
+                            <li><a href="/products">Products</a></li>
                         </ul>
-                    </li>
+                    </li> */}
 
-                    <li className="nav__item nav__item--extra">
-                        <h2 className="nav__title">Account</h2>
-                        <ul className="nav__ul nav__ul--extra">
-                            <li><a href="/auth/signup">Create Account</a></li>
-                            <li><a href="/auth/login">Use Existing Account</a></li>
-                            <li><a href="/dashboard">Dashboard</a></li>
-                            <li><a href="/dashboard/settings/">Settings</a></li>
+                    <li class="nav__item nav__item--extra">
+                        <h2 class="nav__title"><b>Links</b></h2>
+                        <ul class="nav__ul nav__ul--extra">
+                            <li><a href="/">Home</a></li>
+                            <li><a href="/reasearche">Researche</a></li>
+                            <li><a href="/products">Products</a></li>
+                            <li><a href="/blogs">Blogs</a></li>
+                            <li><a href="/partners">Partners</a></li>
                         </ul>
                     </li>
 
                     <li className="nav__item">
-                        <h2 className="nav__title">Legal</h2>
+                        <h2 className="nav__title"><b>Legal</b></h2>
                         <ul className="nav__ul">
                             <li><a href="/privacy-policy">Privacy Policy</a></li>
                             <li><a href="/terms-of-use">Terms of Use</a></li>
                             <li><a href="/sitemap.xml">Sitemap</a></li>
                         </ul>
                     </li>
-                </ul>
+                </ul>&nbsp;
 
                 <div className="legal">
-                    <p>&copy; 2024 <span ><b>Grovix Lab</b></span>. All rights reserved.</p>
+                    <p>&copy; 2024 <span><b>Grovix Lab</b></span>. All rights reserved.</p>
                 </div>
             </footer>
         </>
