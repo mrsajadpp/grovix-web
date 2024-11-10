529ee711ba6515d5718ab3fd9295ce130b1feb5b
mrsajadpp <mrsajadpp@gmail.com>
2024-11-10 12:20:50 +0530

    bug fixed

--- app/robots.ts
+++ app/robots.ts
@@ -6,6 +6,6 @@ export default function robots(): MetadataRoute.Robots {
             userAgent: '*',
             allow: '/',
         },
-        sitemap: 'https://www.cinecast.xyz/sitemap.xml',
+        sitemap: 'https://www.grovixlab.com/sitemap.xml',
     }
 }
